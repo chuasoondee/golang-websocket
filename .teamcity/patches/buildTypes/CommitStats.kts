fac0afd029f8c418744bb3de9b4afd693196f665
admin <admin@TeamCity>
2020-05-08 04:22:37 +0000

    TeamCity change in 'golang-websocket' project: runners of 'commit-stats' build configuration were updated

--- .teamcity/patches/buildTypes/CommitStats.kts
+++ .teamcity/patches/buildTypes/CommitStats.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
@@ -17,6 +18,13 @@ create(DslContext.projectId, BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        exec {
+            name = "publish"
+            path = "%teamcity.tool.git_commit_stats%"
+        }
     }
 
     triggers {
